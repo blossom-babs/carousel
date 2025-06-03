02f0bbb7cb20d260b08e4a08bba62ea52dfb26f7
blossom-babs <saphashb@gmail.com>
2025-06-03 14:02:52 -0400

    mobile responsiveness

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -3,12 +3,12 @@ const CallToAction = () => {
   return (
     <section className=' bg-cozey-sky-bold  campton'>
         <div className='responsive py-12 flex justify-between items-center cta'>
-        <p className='text-fog-soft max-w-[480px] text-2xl font-medium'>Join the Cozey Family to stay ahead on product launches and exclusive content.</p>
+        <p className='text-fog-soft max-w-[480px] text-lg md:text-2xl font-medium'>Join the Cozey Family to stay ahead on product launches and exclusive content.</p>
 
-        <div className='bg-fog-soft rounded-[52px] w-[80%] flex items-center  py-4 max-w-[700px]'>
+        <div className='bg-fog-soft rounded-[52px] w-[80%] flex items-center py-2 md:py-4 max-w-[700px] input-field'>
             <div className="w-[95%] mx-auto flex justify-between">
             <input className='text-midnight-balanced text-lg-semi active:outline-transparent focus:outline-transparent' type="email" placeholder='Email'/>
-            <button className='bg-midnight-bold rounded-[29px] text-fog-soft py-4 px-8 text-lg-semi'>Sign up</button>
+            <button className='bg-midnight-bold rounded-[29px] text-fog-soft py-2 md:py-4 px-4 md:px-8 md:text-lg-semi'>Sign up</button>
             </div>
         </div>
         </div>
